976fa15428081aba795b9d1a4eb34d5c756d0ca9
Amgalanbaatar <amgalanbaatar@Amgalanbaatars-MacBook-Pro.local>
2024-05-31 00:22:33 +0800

    smooth transition added for hero && pen transform when scroll

---
+++ app/components/Hero.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { useRef, useEffect } from "react";
+
+// export const heroAnimation = [
+//   {
+//     class:
+//       "transition-transform duration-[350ms] ease text-[40px] hover:translate-x-[-80%] hover:translate-y-[60%] hover:rotate-[8deg] text-white",
+//   },
+// ];
+
+const rand = (min: any, max: any) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const Hero = () => {
+  const containerRef: any = useRef(null);
+  const containerRef1: any = useRef(null);
+  const containerRef2: any = useRef(null);
+  const containerRef3: any = useRef(null);
+
+  useEffect(() => {
+    const text = [
+      "@",
+      "9",
+      "7",
+      "6",
+      "--",
+      "8",
+      "8",
+      "1",
+      "8",
+      "5",
+      "9",
+      "8",
+      "5",
+    ];
+
+    if (containerRef.current) {
+      containerRef.current.innerHTML = ""; // Clear the container before adding new elements
+
+      text.forEach((value, index) => {
+        const letter = document.createElement("span");
+        letter.className = "letter";
+        letter.innerText = value;
+        // letter.style.animationDelay = `${index * 100}ms`; // Adjust animation delay as needed
+
+        containerRef.current.appendChild(letter);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const text = ["->", "E", "M", "A", "I", "L"];
+
+    if (containerRef1.current) {
+      containerRef1.current.innerHTML = ""; // Clear the container before adding new elements
+
+      text.forEach((value, index) => {
+        const letter = document.createElement("span");
+        letter.className = "letter";
+        letter.innerText = value;
+        // letter.style.animationDelay = `${index * 100}ms`; // Adjust animation delay as needed
+
+        containerRef1.current.appendChild(letter);
+      });
+    }
+  }, []);
+  useEffect(() => {
+    const text = ["->", "I", "N", "S", "T", "A"];
+
+    if (containerRef2.current) {
+      containerRef2.current.innerHTML = ""; // Clear the container before adding new elements
+
+      text.forEach((value, index) => {
+        const letter = document.createElement("span");
+        letter.className = "letter";
+        letter.innerText = value;
+        // letter.style.animationDelay = `${index * 100}ms`; // Adjust animation delay as needed
+
+        containerRef2.current.appendChild(letter);
+      });
+    }
+  }, []);
+  useEffect(() => {
+    const text = ["<", "G", "I", "T", "H", "U", "B", "/", ">"];
+
+    if (containerRef3.current) {
+      containerRef3.current.innerHTML = ""; // Clear the container before adding new elements
+
+      text.forEach((value, index) => {
+        const letter = document.createElement("span");
+        letter.className = "letter";
+        letter.innerText = value;
+        // letter.style.animationDelay = `${index * 100}ms`; // Adjust animation delay as needed
+
+        containerRef3.current.appendChild(letter);
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      <div id="text">
+        <div id="text">
+          <div className="line">
+            <p className="word">G.</p>
+            <p className="word">Amgaa</p>
+          </div>
+
+          <div className="line">
+            <p className="word">DEVELOPER</p>
+            <p className="word">&</p>
+          </div>
+
+          <div className="flex gap-5">
+            {" "}
+            <div className="line">
+              <a
+                id="channel-link"
+                href="https://www.gmail.com/"
+                target="_blank"
+                className="word fancy"
+                ref={containerRef1}
+              ></a>
+            </div>
+            <div className="line">
+              <a
+                id="channel-link"
+                href="https://www.instagram.com/yesoke_snw/"
+                target="_blank"
+                className="word fancy"
+                ref={containerRef2}
+              ></a>
+            </div>
+          </div>
+          <div className="line">
+            <a
+              id="channel-link"
+              href="https://www.instagram.com/yesoke_snw/"
+              target="_blank"
+              className="word fancy"
+              ref={containerRef}
+            ></a>
+          </div>
+          <div className="line flex justify-center items-center">
+            <a
+              id="channel-link"
+              href="https://github.com/Amgalanbaatar888"
+              target="_blank"
+              className="word fancy"
+              ref={containerRef3}
+            ></a>
+            <p className="word">&</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+import { useRef } from "react";
+import { Hero } from "./components/Hero";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
 export default function Home() {
-  return <div>Personal portfolio site</div>;
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress }: any = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["-95%", "1%"]);
+
+  const x2 = useTransform(scrollYProgress, [1, 0], ["0%", "100%"]);
+
+  return (
+    <>
+      <Hero />
+      <div className="w-full h-[1000px] relative">
+        <div
+          ref={targetRef}
+          className="flex sticky top-0  text-white justify-center  mt-32 items-center  "
+        >
+          <motion.div
+            style={{ x }}
+            className="w-[406px] absolute bg-black mt-5 left-[159px] h-[130px]"
+          >
+            <img className="bg-slate-900" src="./images/pen1.png" alt="pen1" />
+          </motion.div>
+
+          <motion.div
+            style={{ x: x2 }}
+            className="w-[1050px]  mt-5 left-[159px] h-[130px]"
+          >
+            <img src="./images/pen2.png" alt="pen2" />
+          </motion.div>
+        </div>
+      </div>
+    </>
+  );
 }
